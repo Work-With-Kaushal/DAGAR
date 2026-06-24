213bb4d310a9b5733a55ace581cfe0cacdbbbfc2
Kaushal Yadav <kaushalendra.kumaryadav.cs.2025@mitmeerut.ac.in>
2026-06-25 01:14:07 +0530

    Connected frontend to Render backend

--- TRINGA/frontend/src/api.js
+++ TRINGA/frontend/src/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const API =
-    axios.create({
-        baseURL:
-            "http://localhost:5000/api"
-    });
+const API = axios.create({
+    baseURL: "https://dagar-backend.onrender.com/api",
+});
 
 export default API;
